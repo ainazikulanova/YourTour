33cd0e8cb47af5ca001656764d8fc7bdb82fba06
Ainazik_Ulanova <159806105+ainazikulanova@users.noreply.github.com>
2024-07-14 13:14:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -75,3 +75,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+const phoneInput = document.getElementById("phone");
+const mask = new IMask(phoneInput, {
+  mask: "+{7}(000)000-00-00",
+});
